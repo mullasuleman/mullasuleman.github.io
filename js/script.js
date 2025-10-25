2716ba9286a07a08f700921f97add649a0e1fe30
mullasuleman <mullasuleman12@gmail.com>
2025-10-25 00:51:29 -0400

    removed resize event

--- js/script.js
+++ js/script.js
@@ -59,15 +59,3 @@ window.addEventListener("DOMContentLoaded", function () {
 		})
 	}
 });
-
-function debounce(func) {
-	var timer;
-	return function (event) {
-		if (timer) clearTimeout(timer);
-		timer = setTimeout(func, 100, event);
-	};
-}
-
-window.addEventListener("resize", debounce(function (e) {
-	window.location.reload();
-}));
